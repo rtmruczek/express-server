2cd1c64efcdd356d01bd2dfd3acc71a1eb35324c
Robert Mruczek <rtmruczek@gmail.com>
2021-07-05 19:00:10 -0400

    discord stuff

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,8 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+  },
   plugins: [reactRefresh()],
 });
